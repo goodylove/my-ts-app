2350f86c6efd47d7d3c3d44699cbd5a7fca1b1ec
goodness <goodyc474@gmail.com>
2023-05-01 11:21:37 +0100

    imported images

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      blue: "#0C4191",
+      white: "#FFFFFF",
+      gray: "#CCDDF6 ",
+      green: "#20FF00",
+      yellow: "#F2F20E",
+      orange: "#FFB300",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
